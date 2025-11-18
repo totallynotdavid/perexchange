0522b70183230c2b6b07238ddc30bc98e36be5a1
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-11-18 07:58:28 -0500

    feat(scrapers): implement fetch_cambioseguro scraper and add tests

--- pkg/core/perexchange/scrapers/__init__.py
+++ pkg/core/perexchange/scrapers/__init__.py
@@ -1,3 +1,4 @@
+from perexchange.scrapers.cambioseguro import fetch_cambioseguro
 from perexchange.scrapers.cuantoestaeldolar import fetch_cuantoestaeldolar
 from perexchange.scrapers.tkambio import fetch_tkambio
 
@@ -5,6 +6,12 @@ from perexchange.scrapers.tkambio import fetch_tkambio
 ALL_SCRAPERS = [
     fetch_cuantoestaeldolar,
     fetch_tkambio,
+    fetch_cambioseguro,
 ]
 
-__all__ = ["ALL_SCRAPERS", "fetch_cuantoestaeldolar", "fetch_tkambio"]
+__all__ = [
+    "ALL_SCRAPERS",
+    "fetch_cambioseguro",
+    "fetch_cuantoestaeldolar",
+    "fetch_tkambio",
+]
