db23f550b5f2dab1787c41ba0ef2646c3760d2b7
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-11-18 08:20:21 -0500

    feat(scrapers): add fetch_tucambista to ALL_SCRAPERS and __all__ exports

--- pkg/core/perexchange/scrapers/__init__.py
+++ pkg/core/perexchange/scrapers/__init__.py
@@ -1,12 +1,14 @@
 from perexchange.scrapers.cambioseguro import fetch_cambioseguro
 from perexchange.scrapers.cuantoestaeldolar import fetch_cuantoestaeldolar
 from perexchange.scrapers.tkambio import fetch_tkambio
+from perexchange.scrapers.tucambista import fetch_tucambista
 
 
 ALL_SCRAPERS = [
     fetch_cuantoestaeldolar,
     fetch_tkambio,
     fetch_cambioseguro,
+    fetch_tucambista,
 ]
 
 __all__ = [
@@ -14,4 +16,5 @@ __all__ = [
     "fetch_cambioseguro",
     "fetch_cuantoestaeldolar",
     "fetch_tkambio",
+    "fetch_tucambista",
 ]
